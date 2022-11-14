eff299b48c4fae5a4a5585bd87013459e241a138
charat <charatkosit@gmail.com>
2022-11-14 15:34:05 +0700

    release0.0.3

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,10 +3,10 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm'
 export const typeOrmConfig: TypeOrmModuleOptions ={
     type: 'mysql',
     host: 'localhost',
-    port: 3306,
+    port: 3308,
     username: 'root',
-    password: '1234567890',
-    database: 'autogp1',
+    password: '1234',
+    database: 'testhaha',
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true,
 }
