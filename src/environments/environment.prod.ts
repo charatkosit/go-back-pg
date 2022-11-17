6be27b606539f4406add7fb31a2238d7c3771191
charat <charatkosit@gmail.com>
2022-11-17 23:13:10 +0700

    release0.0.15

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,9 @@
 /* eslint-disable prettier/prettier */
 export const environment = {
   production: true,
-  dbUrl: "http://goapi.ddns.net:3308/",
-  dbApiToken:"",
-  dbUsername: "root",
-  dbPassword: "1234",
-  dbName: "testhaha",
-  project: "Build Prod:v0.0.1 on 15/11/22",
-  release: "0.0.1",
+  sapUrl: "%sapUrl%",
+  sapApiToken:"%sapApiToken%",
+  baseUrl: "",
+  project: "Build %stage%:%timestamp%",
+  release: "%release%",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,8 @@
-/* eslint-disable prettier/prettier */
 export const environment = {
   production: false,
-  dbUrl: "localhost",
-  dbPort: 3308,
-  dbApiToken:"",
-  dbUsername: "root",
-  dbPassword: "1234",
-  dbName: "testhaha",
-  project: "Build Dev:v0.0.1 on 15/11/22",
-  release: "0.0.1",
+  sapUrl: "http://goapi.ddns.net:3000/api/v1/",
+  sapApiToken:"z@hz3sNY#0ohB9SspeE9@fLDQ%r65x$k8LxL28VH72FfvRWgCn",
+  baseUrl: "",
+  project: "Build Dev:v0.1.3a on 27/10/22",
+  release: "0.0.5",
 };
-
