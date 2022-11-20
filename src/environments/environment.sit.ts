d65bf5393d8336777d5c258a64a01398a9abef55
charat <charatkosit@gmail.com>
2022-11-20 13:48:40 +0700

    release0.0.34

--- src/environments/environment.sit.ts
+++ src/environments/environment.sit.ts
@@ -11,6 +11,6 @@ export const environment = {
     sapApiToken: "z@hz3sNY#0ohB9SspeE9@fLDQ%r65x$k8LxL28VH72FfvRWgCn",
     baseUrl: "",
     project: "Build: SIT",
-    release: "%releaseBS%",
+    release: "0.0.34",
   };
   
